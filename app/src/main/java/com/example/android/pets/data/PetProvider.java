dfb226c2e6d88e0276103b4c906e5efbec4a7931
Muir Halleron <muir.halleron@gmail.com>
2017-06-30 15:10:32 +0100

    Implement notifyChange()

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -159,6 +159,13 @@ public class PetProvider extends ContentProvider {
             default:
                 throw new IllegalArgumentException("Cannot query unknown URI " + uri);
         }
+
+        /**
+         * Set notification URI on the Cursor, so we know what content URI the Cursor was created
+         * for. If the data at this URI changes, then we know we need to update the Cursor.
+         */
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
         return cursor;
     }
 
@@ -190,9 +197,6 @@ public class PetProvider extends ContentProvider {
      */
     @Override
     public Uri insert(@NonNull Uri uri, ContentValues contentValues) {
-
-        validatePet(contentValues);
-
         final int match = uriMatcher.match(uri);
         switch (match) {
             case PETS:
@@ -200,6 +204,34 @@ public class PetProvider extends ContentProvider {
             default:
                 throw new IllegalArgumentException("Insertion is not supported for " + uri);
         }
+    }
+
+    /*
+     * Insert a pet into the database with the given content values. Return the new content URI for
+     * that specific row in the database.
+     */
+    private Uri insertPet(Uri uri, ContentValues contentValues) {
+
+        validatePet(contentValues);
+
+        // Get writable database
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        // Insert the new pet with the given values
+        long id = database.insert(PetEntry.TABLE_NAME, null, contentValues);
+
+        // If the ID is -1, then the insertion failed. Log an error and return null.
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        // Notify all listeners that the data has changed for the pet content URI
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Once we know the ID of the new row in the table, return the new URI with the ID appended
+        // to the end of it.
+        return ContentUris.withAppendedId(uri, id);
     }
 
     public void validatePet(ContentValues contentValues) {
@@ -224,28 +256,6 @@ public class PetProvider extends ContentProvider {
         // No need to check the breed as any value is valid (including null).
     }
 
-    /*
-     * Insert a pet into the database with the given content values. Return the new content URI for
-     * that specific row in the database.
-     */
-    private Uri insertPet(Uri uri, ContentValues contentValues) {
-        // Get writable database
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-
-        // Insert the new pet with the given values
-        long id = database.insert(PetEntry.TABLE_NAME, null, contentValues);
-
-        // If the ID is -1, then the insertion failed. Log an error and return null.
-        if (id == -1) {
-            Log.e(LOG_TAG, "Failed to insert row for " + uri);
-            return null;
-        }
-
-        // Once we know the ID of the new row in the table, return the new URI with the ID appended
-        // to the end of it.
-        return ContentUris.withAppendedId(uri, id);
-    }
-
     /**
      * Delete the data at the given selection and selection arguments.
      *
@@ -260,16 +270,26 @@ public class PetProvider extends ContentProvider {
         // Get writable database
         SQLiteDatabase database = dbHelper.getWritableDatabase();
 
+        // Track the number of rows that were deleted.
+        int rowsDeleted;
+
         final int match = uriMatcher.match(uri);
         switch (match) {
             case PETS:
                 // Delete all rows that match the selection and selection args
-                return database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+                rowsDeleted = database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+                if (rowsDeleted !=0)
+                    getContext().getContentResolver().notifyChange(uri, null);
+
+                return rowsDeleted;
             case PET_ID:
                 // Delete a single row given by the ID in the URI
                 selection = PetEntry._ID + "=?";
                 selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
-                return database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+                rowsDeleted = database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+                if (rowsDeleted !=0)
+                    getContext().getContentResolver().notifyChange(uri, null);
+                return rowsDeleted;
             default:
                 throw new IllegalArgumentException("Deletion is not supported for " +uri);
         }
@@ -290,7 +310,7 @@ public class PetProvider extends ContentProvider {
         final int match = uriMatcher.match(uri);
         switch (match) {
             case PETS:
-                return updatePet(contentValues, selection, selectionArgs);
+                return updatePet(uri, contentValues, selection, selectionArgs);
             case PET_ID:
                 /*
                  * for the PET_ID code, extract out the ID from the URI, so we know which row to
@@ -299,7 +319,7 @@ public class PetProvider extends ContentProvider {
                  */
                 selection = PetEntry._ID + "=?";
                 selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri))};
-                return updatePet(contentValues, selection, selectionArgs);
+                return updatePet(uri, contentValues, selection, selectionArgs);
             default:
                 throw new IllegalArgumentException("Update is not supported for " + uri);
         }
@@ -310,17 +330,23 @@ public class PetProvider extends ContentProvider {
      * specified in the selection and selection arguments (which cold be 0 or 1 or more pets).
      * Return the number of rows that were successfully updated.
      */
-    private int updatePet(ContentValues contentValues, String selection,
+    private int updatePet(Uri uri, ContentValues contentValues, String selection,
                           String[] selectionArgs) {
 
         if (validatePetUpdate(contentValues)) return 0;
 
-
         // Otherwise get writable database to update the data
         SQLiteDatabase database = dbHelper.getWritableDatabase();
 
+        // Perform the update on the database and get the number of rows affected
+        int rowsUpdated = database.update(PetEntry.TABLE_NAME, contentValues, selection, selectionArgs);
+
+        // if 1 or more rows were updated, then notify all listeners that the data has changed
+        if (rowsUpdated !=0)
+            getContext().getContentResolver().notifyChange(uri, null);
+
         // Returns the number of database rows affected by the update statement.
-        return database.update(PetEntry.TABLE_NAME, contentValues, selection, selectionArgs);
+        return rowsUpdated;
     }
 
     public boolean validatePetUpdate(ContentValues contentValues) {
